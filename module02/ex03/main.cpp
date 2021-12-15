1265bbe5bf792f2bbf507270dbde048c91337f62
mshmelly <mshmelly@student.21-school.ru>
2021-12-15 10:53:14 +0300

    ex00 done

--- module02/ex03/main.cpp
+++ module02/ex03/main.cpp
@@ -15,7 +15,7 @@ int main(void)
 	std::cout <<	"C:\t\t"	<<	C	<< std::endl;
 	std::cout <<	"Point1:\t\t"	<<	P	<< std::endl;
 	std::cout <<	"Point2:\t\t"	<<	P2	<< std::endl;
-	std::cout << "\033[1mResult1:\t" << (bsp(A, B, C, P) ? "\033[32mtrue" : "\033[31mfalse") << std::endl;
+	std::cout << "\033[1mResult1:\t" << (bsp(A, B, C, P) ? "ctrue" : "\033[31mfalse") << std::endl;
 	std::cout << "\033[0m\033[1mResult2:\t" << (bsp(A, B, C, P2) ? "\033[32mtrue" : "\033[31mfalse") << std::endl;
 
 	return 0;
