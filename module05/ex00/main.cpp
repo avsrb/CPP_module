c3a5ab2f39a21ba6e1a2364a84d9c15941b4be3b
mshmelly <mshmelly@student.21-school.ru>
2021-12-29 15:13:07 +0300

    module05 ex00 done

--- module05/ex00/main.cpp
+++ module05/ex00/main.cpp
@@ -3,31 +3,102 @@
 
 int	main(void)
 {
-	try
 	{
-		Bureaucrat Alex("Alex", 150);
-		std::cout << Alex << "\n";
-//		Alex.increment();
-//		Alex.increment();
-//		Alex.increment();
-		Alex.decrement();
-		Alex.decrement();
-		Alex.decrement();
-		std::cout << Alex << "\n";
+		try
+		{
+			Bureaucrat Alex("Alex", 150);
+			std::cout << Alex << "\n";
+			Alex.decrement();
+			Alex.decrement();
+			Alex.decrement();
+			std::cout << Alex << "\n";
+		}
+		catch (const Bureaucrat::GradeTooHighException &e)
+		{
+			std::cerr << e._error << " : " << e.what() << std::endl;
+		}
+		catch (const Bureaucrat::GradeTooLowException &e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+		catch (...)
+		{
+			std::cerr << "Something strange happened" << std::endl;
+		}
+	}
+	std::cout << std::endl;
+	{
+		try
+		{
+			Bureaucrat Vasya("Vasya", 150);
+			std::cout << Vasya << "\n";
+			Vasya.increment();
+			Vasya.increment();
+			Vasya.increment();
+			std::cout << Vasya << "\n";
+		}
+		catch (const Bureaucrat::GradeTooHighException &e)
+		{
+			std::cerr << e._error << " : " << e.what() << std::endl;
+		}
+		catch (const Bureaucrat::GradeTooLowException &e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+		catch (...)
+		{
+			std::cerr << "Something strange happened" << std::endl;
+		}
+	}
+	std::cout << std::endl;
+	{
+		try
+		{
+			Bureaucrat Anya("Anya", 1);
+			std::cout << Anya << std::endl;
+			Anya.increment();
+			Anya.increment();
+			Anya.increment();
+			std::cout << Anya << "\n";
+		}
+		catch (const Bureaucrat::GradeTooHighException &e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+		catch (const Bureaucrat::GradeTooLowException &e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+		catch (...)
+		{
+			std::cerr << "Something strange happened" << std::endl;
+		}
+	}
+	std::cout << std::endl;
+	{
+		try
+		{
+			Bureaucrat Anya("Anya", 1);
+			std::cout << Anya << "\n";
+			Anya.decrement();
+			Anya.decrement();
+			Anya.decrement();
+			std::cout << Anya << "\n";
+		}
+		catch (const Bureaucrat::GradeTooHighException &e)
+		{
+			std::cerr << e._error << " : " << e.what() << std::endl;
+		}
+		catch (const Bureaucrat::GradeTooLowException &e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+		catch (...)
+		{
+			std::cerr << "Something strange happened" << std::endl;
+		}
+	}
 
-	}
-	catch(const Bureaucrat::GradeTooHighException &e)
-	{
-		std::cerr << e._error << " : " << e.what() << std::endl;
-	}
-	catch(const Bureaucrat::GradeTooLowException &e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
-	catch(...)
-	{
-		std::cerr << "What the fun is this?" << std::endl;
-	}
 	return (0);
 }
 
