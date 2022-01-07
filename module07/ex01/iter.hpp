9ecb4ef8beb15ed611cbb980751809461d5d64ed
mshmelly <mshmelly@student.21-school.ru>
2022-01-07 18:05:48 +0300

    done m07ex02

--- module07/ex01/iter.hpp
+++ module07/ex01/iter.hpp
@@ -1,3 +1,12 @@
 #pragma once
 
 #include <iostream>
+
+template <typename T>
+void iter(T *arr, int len, void (*func)(T&))
+{
+	for (int i = 0; i < len; ++i)
+	{
+		func(arr[i]);
+	}
+}
--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -1,7 +1,36 @@
 #include "iter.hpp"
 
-int main(void)
+template <typename T>
+void increment(T &t)
 {
+	++t;
+}
+
+
+template <typename T>
+void	printing(T *t, int len)
+{
+	for (int i = 0; i < len; ++i)
+		std::cout << t[i] << ' ';
+	std::cout << std::endl;
+}
+
+int main()
+{
+	char	c[] = {'a', 'b', 'c'};
+	int		i[] = {1, 2, 3};
+	float	f[] = {1.1, 2.2, 3.3};
+
+	::printing(c, 3);
+	::printing(i, 3);
+	::printing(f, 3);
+	::iter(c, 3, increment);
+	::iter(i, 3, increment);
+	::iter(f, 3, increment);
+	std::cout << std::endl;
+	::printing(c, 3);
+	::printing(i, 3);
+	::printing(f, 3);
 
 	return 0;
 }
